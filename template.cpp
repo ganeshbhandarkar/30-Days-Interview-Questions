41ac099bf865ff6fdc1df142c2b184cf99ae164b
ganeshbhandarkar <bhandarkarganesh8181@gmail.com>
2020-06-16 10:12:37 +0530

    Day 1 Arrays

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define tc() int t; cin>>t; while(t--)
+#define MOD 1000000007
+// #define GCD(ll a,ll b) ll gcd(ll a,ll b){if(b==0){return 0;} return gcd(b,b%a)}
+
+void IO(){
+
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	#ifndef OJ
+	freopen("C:/Users/KIIT/Interview Questions/30-Days-Interview-Questions/input.txt","r",stdin);
+	freopen("C:/Users/KIIT/Interview Questions/30-Days-Interview-Questions/output.txt","w",stdout);
+	#endif
+}
+
+void solve(){
+
+
+
+	
+}
+
+int main(){
+
+	IO();
+	
+	tc(){
+		solve();
+	}
+
+	return 0;
+}
